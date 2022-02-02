c08502f0c3a42c8274e0557b2217dd40a06f4e0c
Phillip Burleigh <pburle2@illinois.edu>
2022-02-01 23:24:41 -0600

    Ready files for test release

--- json_decoder.py
+++ json_decoder.py
@@ -1,17 +1,38 @@
 import json
+import sys
+import os
+import platform
 
-vault_path = 'data/vaults.json'
-skills_path = 'data/test.json'
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
 
 def load_json(filepath):
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         return json.load(file)
 
+
 def get_entries(data):
-    return [entry['entryName'] for entry in data['entries']]
+    return [entry["entryName"] for entry in data["entries"]]
+
 
 def get_exits(data, entry):
-    return [exit for exit in data['entries'][entry]['exits']]
+    return [exit for exit in data["entries"][entry]["exits"]]
+
 
 def get_skill_value(data):
-    return [value['value'] for value in data['skills']]
+    return [value["value"] for value in data["skills"]]
+
+vault_path = (
+    resource_path("data/vaults.json")
+    if platform.system() == "Linux"
+    else resource_path("data\\vaults.json")
+)
+skills_path = (
+    resource_path("data/test.json")
+    if platform.system() == "Linux"
+    else resource_path("data\\test.json")
+)
